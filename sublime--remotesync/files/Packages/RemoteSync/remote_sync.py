529d8314ffd4d20171d3299cafff3d35f4101316
Oleg Bulatov <oleg@bulatov.me>
2014-04-12 21:59:50 +0400

    Added LOCAL_POST_COMMAND to RemoteSync
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- sublime--remotesync/files/Packages/RemoteSync/remote_sync.py
+++ sublime--remotesync/files/Packages/RemoteSync/remote_sync.py
@@ -6,7 +6,7 @@ import subprocess
 from threading import Thread
 
 
-def tm_sync_routine(path, filename):
+def _tm_script(path, filename, code, script, success_message):
     p = subprocess.Popen(["/bin/sh", "-s", path, filename],
                          stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE,
@@ -24,81 +24,66 @@ def tm_sync_routine(path, filename):
         : ${REMOTE_USER:=$(whoami)}
         : ${REMOTE_PORT:=22}
         : ${RSYNC_OPTIONS:=}
+    """ + script)
 
-        REMOTE_MACHINE="$REMOTE_USER@$REMOTE_HOST"
-        FILE=${FILENAME/$WORKDIR\//}
-        DIR=`dirname "$REMOTE_PATH/$FILE"`
-
-        RSYNC_CMD="rsync -av --exclude=.tm_sync.config $RSYNC_OPTIONS"
-        ssh -n -p "$REMOTE_PORT" "$REMOTE_MACHINE" mkdir -p "$DIR"
-        $RSYNC_CMD -e "ssh -p $REMOTE_PORT" \\
-            "$WORKDIR/$FILE" "$REMOTE_MACHINE:$REMOTE_PATH/$FILE"
-        RSYNC_RETCODE=$?
-
-        exit $RSYNC_RETCODE
-    """)
     retcode = p.returncode
-
     if retcode is None:
-        sublime.error_message("RemoteSync: unexpected error, rsync still running.\n" + err)
+        sublime.error_message("RemoteSync: unexpected error, " + code +
+                              " still running.\n" + err)
+        return False
     elif retcode != 0:
-        sublime.error_message("RemoteSync: rsync failed.\n" + err)
-    else:
-        def done():
-            sublime.status_message("File " + filename + " synced")
-        sublime.set_timeout(done, 0)
-        tm_post_command_routine(path, filename)
+        sublime.error_message("RemoteSync: " + code + " failed.\n" + err)
+        return False
 
-
-def tm_post_command_routine(path, filename):
-    p = subprocess.Popen(["/bin/sh", "-s", path, filename],
-                         stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                         universal_newlines=True)
-    (out, err) = p.communicate("""
-        DISABLE=
-        WORKDIR=$1
-        FILENAME=$2
-        . "$WORKDIR/.tm_sync.config"
-
-        if [[ -n "$DISABLE" ]]; then
-            exit
-        fi
-
-        : ${REMOTE_USER:=$(whoami)}
-        : ${REMOTE_PORT:=22}
-        : ${RSYNC_OPTIONS:=}
-
-        if [[ -n "$REMOTE_POST_COMMAND" ]]; then
-            ssh -f -p "$REMOTE_PORT" "$REMOTE_USER@$REMOTE_HOST" -- \
-                "cd \"$REMOTE_PATH\" && $REMOTE_POST_COMMAND"
-        fi
-    """)
-    retcode = p.returncode
-
-    if retcode is None:
-        sublime.error_message("RemoteSync: unexpected error, remote command still running.\n" + err)
-    elif retcode != 0:
-        sublime.error_message("RemoteSync: remote command failed.\n" + err)
-    else:
-        def done():
-            sublime.status_message("Remote command completed successfully")
-        sublime.set_timeout(done, 0)
+    def done():
+        sublime.status_message(success_message)
+    sublime.set_timeout(done, 0)
+    return True
 
 
 class TmSyncThread(Thread):
-
     def __init__(self, path, filename):
         Thread.__init__(self)
         self.path = path
         self.filename = filename
 
     def run(self):
-        tm_sync_routine(self.path, self.filename)
+        done = _tm_script(self.path, self.filename, "rsync", """
+            REMOTE_MACHINE="$REMOTE_USER@$REMOTE_HOST"
+            FILE=${FILENAME/$WORKDIR\//}
+            DIR=`dirname "$REMOTE_PATH/$FILE"`
+
+            RSYNC_CMD="rsync -av --exclude=.tm_sync.config $RSYNC_OPTIONS"
+            ssh -n -p "$REMOTE_PORT" "$REMOTE_MACHINE" mkdir -p "$DIR"
+            $RSYNC_CMD -e "ssh -p $REMOTE_PORT" \\
+                "$WORKDIR/$FILE" "$REMOTE_MACHINE:$REMOTE_PATH/$FILE"
+            RSYNC_RETCODE=$?
+
+            exit $RSYNC_RETCODE
+        """, "File " + self.filename + " synced")
+        if not done:
+            return
+
+        done = _tm_script(self.path, self.filename, "local command", """
+            if [[ -n "$LOCAL_POST_COMMAND" ]]; then
+                cd "$WORKDIR"
+                eval "$LOCAL_POST_COMMAND"
+            fi
+        """, "Local command completed successfully")
+        if not done:
+            return
+
+        done = _tm_script(self.path, self.filename, "remote command", """
+            if [[ -n "$REMOTE_POST_COMMAND" ]]; then
+                ssh -f -p "$REMOTE_PORT" "$REMOTE_USER@$REMOTE_HOST" -- \
+                    "cd \"$REMOTE_PATH\" && $REMOTE_POST_COMMAND"
+            fi
+        """, "Remote command completed successfully")
+        if not done:
+            return
 
 
 class RsyncOnSave(sublime_plugin.EventListener):
-
     def run_tm_sync(self, view, path):
         thread = TmSyncThread(path, view.file_name())
         thread.start()
@@ -114,7 +99,7 @@ class RsyncOnSave(sublime_plugin.EventListener):
                 self.run_tm_sync(view, dirname)
                 return
 
-            next = os.path.dirname(dirname)
-            if next == dirname:
+            next_dirname = os.path.dirname(dirname)
+            if next_dirname == dirname:
                 break
-            dirname = next
+            dirname = next_dirname
